818c4b3bf5fdce1aada60f678b325127b277dcdb
Moinak Samaddar <111280341+moinakhere@users.noreply.github.com>
2024-11-24 15:25:37 +0530

    Add files via upload

---
+++ Tic tac toe.py
@@ -0,0 +1,42 @@
+n=0
+l=[]
+print('Player 1: X\nPlayer 2: O')
+for i in range(9):
+    l.append(i)
+print('', l[0], '|', l[1], '|', l[2], '\n-----------\n', l[3], '|', l[4], '|', l[5], '\n-----------\n', l[6], '|', l[7], '|', l[8])
+l1=[]
+l2=[]
+while n!=1:
+    a = int(input('Player 1, enter your move:'))
+    l1.append(a)
+    if a in l2:
+        print('Invalid move.')
+    else:
+        for i in range(9):
+            if a == l[i]:
+                l.pop(a)
+                l.insert(a,'X')
+        print('', l[0], '|', l[1], '|', l[2], '\n-----------\n', l[3], '|', l[4], '|', l[5], '\n-----------\n', l[6], '|', l[7], '|', l[8])
+        if (l[0]==l[1]==l[2]=='X') or (l[3]==l[4]==l[5]=='X') or (l[6]==l[7]==l[8]=='X') or (l[0]==l[3]==l[6]=='X') or (l[1]==l[4]==l[7]=='X') or (l[2]==l[5]==l[8]=='X') or (l[0]==l[4]==l[8]=='X') or (l[2]==l[4]==l[6]=='X'):
+            print('Player 1 is the winner!')
+            break
+        elif l[0]!=0 and l[1]!=1 and l[2]!=2 and l[3]!=3 and l[4]!=4 and l[5]!=5 and l[6]!=6 and l[7]!=7 and l[8]!=8:
+            if (l[0]!=l[1]!=l[2]) or (l[3]!=l[4]!=l[5]) or (l[6]!=l[7]!=l[8]) or (l[0]!=l[3]!=l[6]) or (l[1]!=l[4]!=l[7]) or (l[2]!=l[5]!=l[8]) or (l[0]!=l[4]!=l[8]) or (l[2]!=l[4]!=l[6]):
+                print('Nice game, but no one won')
+                break
+            else:
+                print('Nice game, but no one won')
+                break
+    b = int(input('Player 2, enter your move:'))
+    l2.append(b)
+    if b in l1:
+        print('Invalid move.')
+    else:
+        for i in range(9):
+            if b == l[i]:
+                l.pop(b)
+                l.insert(b,'O')
+        print('', l[0], '|', l[1], '|', l[2], '\n-----------\n', l[3], '|', l[4], '|', l[5], '\n-----------\n', l[6], '|', l[7], '|', l[8])
+        if (l[0]==l[1]==l[2]=='O') or (l[3]==l[4]==l[5]=='O') or (l[6]==l[7]==l[8]=='O') or (l[0]==l[3]==l[6]=='O') or (l[1]==l[4]==l[7]=='O') or (l[2]==l[5]==l[8]=='O') or (l[0]==l[4]==l[8]=='O') or (l[2]==l[4]==l[6]=='O'):
+            print('Player 2 is the winner!')
+            break
